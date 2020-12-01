97eb5cf51f95bf39c6751b1be4221d7a603e2e82
Davdi <n.davdi@gmail.com>
2020-12-01 23:23:23 +0100

    1.3

--- clock.js
+++ clock.js
@@ -1,15 +1,6 @@
-const dateNow = new Date();
-console.log(dateNow);
-const datehms = new Date(2020, 11, 1, 20, 29, 10, 555);
-console.log(datehms);
-
-const dateString = Date();
-console.log(dateString);
-
-console.log(dateNow.toLocaleTimeString('hu'));
-
-console.log(dateNow.getHours());
-console.log(dateNow.getMinutes());
-console.log(dateNow.getSeconds());
-
-function realTime()
+const clock = setInterval(clocking, 1000);
+function clocking() {
+  let d = new Date();
+  let t = d.toLocaleTimeString();
+  document.querySelector(".clock").innerText = t;
+};
